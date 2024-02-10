082dbc250bcc40821f022baddcbe5e4ec8b95443
Anamika Kumari <122178637+Anamika-K20@users.noreply.github.com>
2024-02-10 12:45:09 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,17 +4,20 @@ import React from "react";
 import "./HeroSection.css"; // Import your CSS file
 
 const HeroSection: React.FC = () => {
+  const backgroundImageUrl =
+    "https://www.fortishealthcare.com/drupal-data/styles/banner_image/azblob/2023-03/New-Web-Banner-03.jpg?itok=cxkOaBxp";
+
   return (
-    <section className="hero-section">
+    <section
+      className="hero-section"
+      style={{ backgroundImage: `url(${backgroundImageUrl})` }}
+    >
       <div className="hero-content">
         <h1>Medifam Connect: Your Family's Health, Simplified</h1>
         <p>Streamline and organize your family's health records with ease.</p>
         <a href="#signup" className="cta-button">
           Get Started
         </a>
-      </div>
-      <div className="hero-image">
-        <img src="/path/to/family-image.jpg" alt="Happy and Healthy Family" />
       </div>
     </section>
   );
